0c91bff550ea1680c73489ea2c4a43073ffe9423
Daniel Farah <danielfarah54@gmail.com>
2021-07-23 10:43:29 -0300

    fix: fix PORT

--- bin/server.js
+++ bin/server.js
@@ -2,7 +2,7 @@ const app = require('../src/app')
 const http = require('http')
 const debug = require('debug')('daniel:server')
 
-const port = normalizePort(process.env.port || '3000')
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 const server = http.createServer(app)
